09a082fc4d3bd4390cc8739eb78d8f7ef238e054
Muhammad Bin Nasir <notsogoofybro@gmail.com>
2025-06-20 17:39:11 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    appDir: true,  // ✅ Tell Next.js you are using App Router
+  },
 };
 
 export default nextConfig;
+
